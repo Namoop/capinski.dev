f3bfe8c8cbe33d0c50c397832b495d852edf07ed
theoc <theo1cap@gmail.com>
2022-12-07 14:44:04 -0800

    halfway done with d7

--- src/routes/aoc2022/day7/+page.svelte
+++ src/routes/aoc2022/day7/+page.svelte
@@ -1,12 +1,75 @@
 <script lang="ts">
-	export let data: { input: string };
+	export let data: { input: string; testinput: string };
 	const copy = () => navigator.clipboard.writeText(String(result));
 
 	const input = data.input.split("\n");
 
-	const result = input;
+	type Filesystem = {
+		dirs: {
+			[index: string]: Filesystem; // subdirectories
+		};
+		files: {
+			[index: string]: number; // size
+		};
+		parent: Filesystem;
+		size: number;
+	};
+	const TOTAL_DISK_SPACE = 70000000;
+	const REQUIRED_FREE_SPACE = 30000000;
+	const filesystem: Filesystem = {
+		parent: null,
+		dirs: {},
+		files: {},
+		size: 0,
+	};
+	const below100k: Set<Filesystem> = new Set();
 
-	const code = ``.replaceAll("\n", "<br>").replaceAll("\t", "&nbsp&nbsp")
+	let smallestDirOfMinimumSize: filesystem | undefined;
+
+	let parent;
+	input.forEach((value, index) => {
+		if (value[0] == "$") {
+			if (value[2] == "c") {
+				//command --> cd x
+				const goto = value.slice(5);
+				if (goto == "/") parent = filesystem;
+				else if (goto == "..") parent = parent.parent;
+				else {
+					parent.dirs[goto] ??
+						(parent.dirs[goto] = {
+							parent: parent,
+							dirs: {},
+							files: {},
+							size: 0,
+						});
+					parent = parent.dirs[goto];
+					below100k.add(parent);
+				}
+			}
+			return;
+		}
+		if (value[0] != "d") {
+			//if is NOT a directory --> is a file
+			const [size, name] = value.split(" ");
+			parent.files[name] = +size;
+			let tempparent = parent;
+			while (tempparent) {
+				tempparent.size += +size;
+				if (tempparent.size > 100000) below100k.delete(tempparent);
+				tempparent = tempparent.parent;
+			}
+		}
+	});
+
+	const sumOfAllBelow10k = [...below100k].reduce((c, v) => c + v.size, 0); //result 1
+
+	const NEED_TO_CLEAR = TOTAL_DISK_SPACE - filesystem.size;
+
+	console.log(filesystem);
+	console.log(below100k);
+	const result = sumOfAllBelow10k; //filesystem;
+
+	const code = ``.replaceAll("\n", "<br>").replaceAll("\t", "&nbsp&nbsp");
 </script>
 
 <!-- prettier-ignore -->

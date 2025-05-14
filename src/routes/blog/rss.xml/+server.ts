ba680cb44401060c8e98f2d4f4bbc8cfa9719d32
theodore <theo1cap@gmail.com>
2025-05-13 21:47:55 -0700

    feat: rss feed

---
+++ src/routes/blog/rss.xml/+server.ts
@@ -0,0 +1,56 @@
+import type {Actions, PageServerLoad} from "../../../../.svelte-kit/types/src/routes/$types";
+import PocketBase from "pocketbase";
+import type {Cookies} from "@sveltejs/kit";
+export const prerender = true;
+
+export async function GET() {
+    const headers = {
+        'Content-Type': 'application/xml',
+        'Cache-Control': 'max-age=0, s-maxage=3600'
+    };
+
+    const pb = new PocketBase('https://pocketbase.capinski.dev');
+    const EXCLUDE_PAGES = ['home', 'new'] // TODO refactor pages into separate collection
+
+    // fetch blogs from pocketbase
+    let blogs;
+    try {
+        blogs = await pb.collection('pages').getFullList({
+            sort: '-pub_date',
+            filter: ('published=true && ') + EXCLUDE_PAGES.map(page => `page!="${page}"`).join(' && '),
+            expand: 'thumbnail',
+        });
+    } catch (e) {
+        console.log(e);
+        return {status: 500};
+    }
+
+
+    const xml = `<?xml version="1.0" encoding="UTF-8" ?>
+	<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+	    <title>Capinski.Dev</title>
+	    <description>With great power comes high-voltage physics shenanigans and a surge of current procrastination.</description>
+	    <link>https://www.capinski.dev/</link>
+	    <atom:link href="https://www.capinski.dev/blog/rss.xml" rel="self" type="application/rss+xml"/>
+	    <language>en</language>
+	    ${blogs
+        .map(
+            (post) => `
+	      <item>
+	        <title>${post.title}</title>
+	        <link>https://www.capinski.dev/blog/${post.page}</link>
+	        <guid>https://www.capinski.dev/blog/${post.page}</guid>
+	        <pubDate>${new Date(post.pub_date).toUTCString()}</pubDate>
+	        <author>Theodore Capinski</author>
+	        <description>${post.summary}</description>
+	        <enclosure url="${post.thumbnail ? pb.getFileUrl(post, post.thumbnail) : ''}" type="image/png" />
+	      </item>
+	    `
+        )
+        .join('')}
+    </channel>
+	</rss>`;
+
+    return new Response(xml, { headers });
+}
